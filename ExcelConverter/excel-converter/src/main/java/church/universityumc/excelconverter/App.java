0724f5d542c735cc77190d22b26868ec102b089f
John Lusk <john-github@how-hard-can-it-be.com>
2018-01-31 21:51:55 -0500

    Cmd-line parsing, can specify file on cmd line.

--- ExcelConverter/excel-converter/src/main/java/church/universityumc/excelconverter/App.java
+++ ExcelConverter/excel-converter/src/main/java/church/universityumc/excelconverter/App.java
@@ -3,8 +3,15 @@ package church.universityumc.excelconverter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFDataFormat;
@@ -27,26 +34,59 @@ import org.apache.poi.ss.util.CellRangeAddress;
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException
+   private static void showHelp() {
+      String help =
+            "Reformat Excel file.\n\n"
+            + "-h\thelp\n"
+            + "-f\tinput file\n"     
+            ;
+      System.out.println( help);
+   }
+   
+    public static void main( String[] args ) throws IOException, ParseException
     {
-        dumpExcelFile();
+       CommandLine cmdLine = parseCommandLine( args);
+       if (cmdLine.hasOption( 'h'))
+          showHelp();
+       String infileName = "test.xls";
+       if (cmdLine.hasOption( 'f'))
+          infileName = cmdLine.getOptionValue( 'f');
+       dumpExcelFile( infileName);
     }
     
-    private static void dumpExcelFile() throws IOException {
-       String filename = "test.xls";
-       HSSFWorkbook workbook = readFile( filename);
+    
+    private static CommandLine parseCommandLine( String[] args) throws ParseException
+    {
+       Options options = new Options();
+       options.addOption( "h", "show help");
+       options.addOption( "f", true, "input file");
+       
+       CommandLineParser parser = new DefaultParser();
+       CommandLine cmdLine = parser.parse( options, args);
+       return cmdLine;
+    }
+    
+    private static void dumpExcelFile(String anInfileName) throws IOException {
+       HSSFWorkbook workbook = readFile( anInfileName);
        for (Sheet sheet : workbook) // For all sheets in the workbook...
           for (Row row : sheet) // For all rows in the sheet...
              for (Cell cell : row) { // For all cells in the row...
                 Font font = workbook.getFontAt( cell.getCellStyle().getFontIndex());
-                String format;
+                List<String> formats = new ArrayList<String>();
                 if (font.getBold())
-                   format = " (bold)";
-                else
+                   formats.add( "bold");
+                if (font.getItalic())
+                   formats.add( "italic");
+                String format;
+                if (formats.isEmpty())
                    format = "";
-                System.out.println( "got: " + cell.getStringCellValue() + format /* + "\t" + font */ );
+                else
+                   format = " (" + String.join( " ", formats) + ")";
+                System.out.println( "got: " + cell.getStringCellValue() + " " + format /* + "\t" + font */ );
              }
     }
+    
+    
     
     private static HSSFWorkbook readFile(String filename) throws IOException {
        try (FileInputStream fis = new FileInputStream(filename)) {
