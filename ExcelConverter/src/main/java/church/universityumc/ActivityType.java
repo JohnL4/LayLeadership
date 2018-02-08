78a081b3d3f53c5ce8621616eba23a3caeef367d
John Lusk <john-github@how-hard-can-it-be.com>
2018-02-08 18:31:58 -0500

    Begin ActivityType parse/store.

--- ExcelConverter/src/main/java/church/universityumc/ActivityType.java
+++ ExcelConverter/src/main/java/church/universityumc/ActivityType.java
@@ -1,13 +1,28 @@
 package church.universityumc;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class ActivityType
 {
+   private static final Pattern YEAR_RE = Pattern.compile( "\\b\\d{4}\\b");
+   private static final Pattern COMMITTEE_RE = Pattern.compile( "\\bcommittees?\\b", Pattern.CASE_INSENSITIVE);
+   private static final Pattern MONTH_RE = Pattern.compile(
+         "\\b(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|june?|july?|aug(ust)?|sep(t(ember)?)?|oct(ober)?|nov(ember)?|dec(ember)?)\\b",
+         Pattern.CASE_INSENSITIVE);
+
+   /**
+    * Map from {@link #name} to {@link ActivityType}.
+    */
+   private static Map<String,ActivityType> allActivityTypes = new HashMap<String,ActivityType>();
+   
    private String name;
 
    public static ActivityType find( String anActivityType)
    {
+      
       // TODO Auto-generated method stub -- find/add in static set.
       return null;
    }
