296e3ecd293b2a81467c754ddae3f39b0bf8234b
John Lusk <john-github@how-hard-can-it-be.com>
2020-03-30 21:34:37 -0400

    Got a really stupid servlet to fwd to a stupid JSP

---
+++ LLWebJ/Web/src/main/java/com/how_hard_can_it_be/layleadership/servlet/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.how_hard_can_it_be.layleadership.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+   /**
+    *
+    */
+   private static final long serialVersionUID = 1L;
+
+   public void doGet(HttpServletRequest aRequest, HttpServletResponse aResponse) 
+      throws ServletException, IOException
+   {
+      aRequest.getServletContext().getRequestDispatcher("/index.jsp").forward(aRequest, aResponse);
+   }
+
+   public static void main(String[] args)
+   {
+      System.out.println( "Hi!");
+   }
+}
---
+++ LLWebJ/Web/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p>
+   From <%= request.getRequestURL() %>
+</p>
+</body>
+</html>
